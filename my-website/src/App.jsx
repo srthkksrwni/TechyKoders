c0d6c8b0d5c3c75c4e345c9d6a16915f52c95ee6
NIkhil Sahu <117375980+nikhilsahu2002@users.noreply.github.com>
2024-04-05 00:33:57 +0530

    commit

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -2,17 +2,15 @@ import React from "react";
 
 // import { Router, Route } from 'react-router-dom';
 import Home from "../public/pages/Home";
-import Sign from "../public/Auth/Sign";
-import SignUpForm from "../public/Auth/SignUpForm";
 // import Footer from "../public/componenets/Footer";
 // import Hero from "../public/componenets/Hero";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <Sign />
-      {/* <SignUpForm /> */}
+      <Home />
+      {/* <Footer/> */}
+      {/* <Hero/> */}
     </>
   );
 }
