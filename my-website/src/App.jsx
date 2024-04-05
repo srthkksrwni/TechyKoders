77de5ee9fdf125771d51291f754922e2dc7c320b
srthkksrwni <109288118+srthkksrwni@users.noreply.github.com>
2024-04-05 19:30:02 +0530

    add

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -1,32 +1,11 @@
 import React from "react";
 
-<<<<<<< HEAD
 
-// import Home from "../public/pages/Home";
-
-
-=======
-// import { Router, Route } from 'react-router-dom';
-import Home from "../public/pages/Home";
-// import Footer from "../public/componenets/Footer";
-// import Hero from "../public/componenets/Hero";
-import Table from "../public/Dashboard/Table";
-import Programes from "../public/componenets/Programes";
->>>>>>> 8c2fca839fb0379b047ab721afd508b8f99e162b
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-            {/* <Home /> */}
-        c
-=======
-      {/* <Home /> */}
-      {/* <Footer/> */}
-      {/* <Hero/> */}
-      {/* <Table /> */}
-      <Programes />
->>>>>>> 8c2fca839fb0379b047ab721afd508b8f99e162b
+          
     </>
   );
 }
